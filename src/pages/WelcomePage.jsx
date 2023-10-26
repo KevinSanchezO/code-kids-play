479a76fe6a10ab2fad2af87087a4f856e0ab5371
Kevin Sánchez Obando <kesanchezo@gmail.com>
2023-10-26 00:33:35 -0600

    Modified the presentation text of the welcome page

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -7,10 +7,15 @@ export const WelcomePage = () => {
         Bienvenido a Code Kids Play
       </h1>
 
+      <p className="d-flex justify-content-center mt-4">
+        En este portal tendras la posibilidad de aprender de manera simple teoría sobre programación basica y adentrarte
+      </p>
       <p className="d-flex justify-content-center">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis gravida vehicula ornare. Etiam euismod varius eleifend. Proin id nisi elit. <br />
-        Duis tincidunt consectetur dignissim. Aliquam finibus dictum porttitor. Ut vel tincidunt nulla. Proin a auctor sapien. <br />
-        Curabitur eget malesuada risus. Sed vel ornare nibh. Sed ultrices sed diam non dictum. Curabitur venenatis magna sit amet commodo dignissim. <br />
+        más en el mundo del desarrollo de software con ejemplos simples y un ejercicio final muy divertido de realizar.
+      </p>
+      <p className="d-flex justify-content-center">
+
+        ¡Entra, lo pasaras genial. Recuerda seguir las lecciones en orden y nunca parar de aprender!
       </p>
 
       <hr />
